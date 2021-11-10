8062fc71002f69ccef14e1b7afc6d0473e0a1c7c
chemsosft1 <yusufabdulrazaq7@gmail.com>
2021-11-10 23:52:03 +0100

    new mine project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// Lets practice props and mapping on our todo list app
+// I've created a js file with some todo data in it, normally this data would come from an Api
+// challenge: using the array map Method, render a child Component for each todo item in the todosData
+// array and pass the relevant data to it.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+
+import TodoItem from './TodoItem';
+import './style.css'
+import TodosData from './TodosData';
+
+
+class App extends React.Component {
+    constructor(){
+        super()
+    this.state = {
+        todos : TodosData
+    }
+        this.handleTodos = this.handleTodos.bind(this)
+    }
+       handleTodos(id) {
+      this.setState(prevState => {
+          const updatedTodos = prevState.todos.map(todo => {
+              if(todo.id === id) {
+                  todo.completed = !todo.completed
+              }
+              return todo
+          })
+          return {
+              todos : updatedTodos
+          }
+      })
+       }
+
+    render(){
+        const todos = TodosData.map( item => {
+            return <TodoItem item={item} key={item.id} handleTodos={this.handleTodos} />
+    })
+    return(
+        <div className="todo-list">
+           {todos}
+        </div>
+    );
+    }
+};
 
 export default App;
---
+++ src/TodosData.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const TodosData = [
+    {
+        id : '1',
+        text : 'Take out the trash',
+        completed : true
+    },
+
+    {
+        id : '2',
+        text : 'Grocery Shopping',
+        completed : false
+    },
+
+    {
+        id : '3',
+        text : 'Take my baby out',
+        completed : false
+    },
+
+    {
+        id : '4',
+        text : 'Make some crypto transactions',
+        completed : true
+    },
+
+    {
+        id : '5',
+        text : 'Go to the Gym',
+        completed : false
+    },
+
+    {
+        id : '6',
+        text : '2hours of rugged prayers',
+        completed : false
+    }
+]
+
+export default TodosData;
